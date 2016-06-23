2b61c6b4bb59f0e2e7e80345984c05e716207f92
jszymanowski <j.szymanowski@gmail.com>
2016-06-23 14:36:46 -0400

    Implementing Topic model tests

--- tests/server/models/topic-test.js
+++ tests/server/models/topic-test.js
@@ -7,28 +7,82 @@ var db = require('../../../server/db');
 
 var User = db.model('user');
 var Topic = db.model('topic');
-var Prerequisite = db.model('prerequisite');
+var PrerequisiteTopic = db.model('PrerequisiteTopic');
+// Topic.belongsToMany(Topic, {as:'prerequisite', through: 'PrerequisiteTopic'});
 
 describe('Topic model', function () {
 
     beforeEach('Sync DB', function () {
-       return db.sync({ force: true });
+      return db.sync({ force: true });
     });
 
     describe('creating topics', function () {
-      describe('can create a topic with a title and description')
-      describe('cannot create a topic with a blank title')
-      describe('cannot create a topic with a blank description')
+
+      it('can create a topic with a title and description', function() {
+        var validTopic = {
+          title: 'JavaScript',
+          description: 'Greatest programming language ever, or greatest language ever?'
+        };
+        Topic.create(validTopic)
+        .then( function(newTopic) {
+          expect(newTopic.title).to.equal(validTopic.title);
+        });
+      });
+
+      it('cannot create a topic with a blank title', function() {
+        Topic.create({ description: 'Something' })
+        .then( function(newTopic) {
+          expect(newTopic).to.be.undefined;
+        });
+      });
+
+      it('cannot create a topic with a blank description', function() {
+        Topic.create({ title: 'A title' })
+        .then( function(newTopic) {
+          expect(newTopic).to.be.undefined;
+        });
+      });
     });
 
     describe('topic associations', function () {
-      describe('can add a prerequisite topic')
-      describe('can add a subsequent topic')
+
+      var baseTopic;
+
+      beforeEach('Add a base topic', function() {
+        var validTopic = {
+          title: 'Node',
+          description: 'Master of the serveriverse?'
+        };
+        baseTopic = Topic.create(validTopic);
+      });
+
+      it('can add a prerequisite topic', function() {
+        var topic = {
+          title: 'JavaScript',
+          description: 'Greatest programming language ever, or greatest language ever?'
+        };
+        Topic.create(topic)
+        .then( function(newTopic) {
+          return baseTopic.addPrerequisite(newTopic);
+        })
+        .then( function(x) {
+
+        });
+      });
+
+      it('can add a subsequent topic', function() {
+
+      })
     });
 
     describe('prerequisite ratings', function() {
-      describe('prerequisite-topic relationship upvote rating is 0 by default')
-      describe('can upvote a prerequisite-topic relationship')
+      it('prerequisite-topic relationship upvote rating is 0 by default', function() {
+
+      });
+
+      it('can upvote a prerequisite-topic relationship', function() {
+
+      });
     })
 
 });
--- tests/server/routes/topic-route-test.js
+++ tests/server/routes/topic-route-test.js
@@ -15,25 +15,45 @@ describe('Topic routes', function () {
     });
 
     describe('creating topics', function () {
-      describe('any user can create a topic')
-      describe('user-created topics should be pending status')
-      describe('admin can approve topics created by ordinary users')
-      describe('topics approved by admins are visible by ordinary users')
+      describe('any user can create a topic', function() {
+
+      })
+      describe('user-created topics should be pending status', function() {
+
+      })
+      describe('admin can approve topics created by ordinary users', function() {
+
+      })
+      describe('topics approved by admins are visible by ordinary users', function() {
+
+      })
     });
 
     describe('retrieving topics', function () {
-      describe('users can see all approved topics')
-      describe('user cannot see pending topics')
+      describe('users can see all approved topics', function() {
+
+      })
+      describe('user cannot see pending topics', function() {
+
+      })
     });
 
     describe('editing and deleting topics', function () {
-      describe('only admins can edit topics')
-      describe('only admins can delete topics')
+      describe('only admins can edit topics', function() {
+
+      })
+      describe('only admins can delete topics', function() {
+
+      })
     });
 
     describe('topic associations', function () {
-      describe('can pull all prerequisites with a topic')
-      describe('can pull all subsequent topics with a topic')
+      describe('can pull all prerequisites with a topic', function() {
+
+      })
+      describe('can pull all subsequent topics with a topic', function() {
+
+      })
     });
 
 });
