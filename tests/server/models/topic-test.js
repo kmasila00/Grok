47ce01f52fef993234895eb8e5684e8920d95886
jszymanowski <j.szymanowski@gmail.com>
2016-06-23 15:19:51 -0400

    Completed Topic model tests

--- tests/server/models/topic-test.js
+++ tests/server/models/topic-test.js
@@ -5,10 +5,8 @@ var Sequelize = require('sequelize');
 
 var db = require('../../../server/db');
 
-var User = db.model('user');
 var Topic = db.model('topic');
 var PrerequisiteTopic = db.model('PrerequisiteTopic');
-// Topic.belongsToMany(Topic, {as:'prerequisite', through: 'PrerequisiteTopic'});
 
 describe('Topic model', function () {
 
@@ -23,23 +21,27 @@ describe('Topic model', function () {
           title: 'JavaScript',
           description: 'Greatest programming language ever, or greatest language ever?'
         };
-        Topic.create(validTopic)
+        return Topic.create(validTopic)
         .then( function(newTopic) {
           expect(newTopic.title).to.equal(validTopic.title);
         });
       });
 
       it('cannot create a topic with a blank title', function() {
-        Topic.create({ description: 'Something' })
-        .then( function(newTopic) {
-          expect(newTopic).to.be.undefined;
+        var invalidTopic = Topic.build({ description: 'Description of something' });
+        return invalidTopic.validate()
+        .then( function (err) {
+          expect(err).to.be.an('object');
+          expect(err.message).to.equal('notNull Violation: title cannot be null');
         });
       });
 
       it('cannot create a topic with a blank description', function() {
-        Topic.create({ title: 'A title' })
-        .then( function(newTopic) {
-          expect(newTopic).to.be.undefined;
+        var invalidTopic = Topic.build({ title: 'A title' });
+        return invalidTopic.validate()
+        .then( function (err) {
+          expect(err).to.be.an('object');
+          expect(err.message).to.equal('notNull Violation: description cannot be null');
         });
       });
     });
@@ -53,7 +55,11 @@ describe('Topic model', function () {
           title: 'Node',
           description: 'Master of the serveriverse?'
         };
-        baseTopic = Topic.create(validTopic);
+        return Topic.create(validTopic)
+        .then(function(newTopic) {
+          baseTopic = newTopic;
+          return newTopic;
+        });
       });
 
       it('can add a prerequisite topic', function() {
@@ -61,28 +67,36 @@ describe('Topic model', function () {
           title: 'JavaScript',
           description: 'Greatest programming language ever, or greatest language ever?'
         };
-        Topic.create(topic)
+        return Topic.create(topic)
         .then( function(newTopic) {
           return baseTopic.addPrerequisite(newTopic);
         })
-        .then( function(x) {
-
+        .then( function() {
+          return baseTopic.getPrerequisite();
+        })
+        .then( function(prerequisites) {
+          expect(prerequisites.length).to.equal(1);
+          expect(prerequisites[0].title).to.equal(topic.title);
         });
       });
 
       it('can add a subsequent topic', function() {
-
+        var topic = {
+          title: 'Express',
+          description: 'Runs your server more expressly.'
+        }
+        return Topic.create(topic)
+        .tap( function(newTopic) {
+          return newTopic.addPrerequisite(baseTopic);
+        })
+        .then( function(newTopic) {
+          return newTopic.getPrerequisite();
+        })
+        .then( function(prerequisites) {
+          expect(prerequisites.length).to.equal(1);
+          expect(prerequisites[0].title).to.equal(baseTopic.title);
+        });
       })
     });
-
-    describe('prerequisite ratings', function() {
-      it('prerequisite-topic relationship upvote rating is 0 by default', function() {
-
-      });
-
-      it('can upvote a prerequisite-topic relationship', function() {
-
-      });
-    })
 
 });
