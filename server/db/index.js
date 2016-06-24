5ee9ad853a770bb0e6003df517f9c094fb689ba7
Michael Chua <mchua1291@gmail.com>
2016-06-24 14:10:44 -0400

    fixed server bug

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,6 @@ module.exports = db;
 
 var User = require('./models/user');
 var Topic = require('./models/topic');
-var Prerequisite= require('./models/prerequisites');
 var Resource = require('./models/resource');
 var Plan = require('./models/plan');
 var Tag = require('./models/tag');
