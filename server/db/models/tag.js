5696041d30be1ae10bc32d60b7270e76d892599d
Angel <rriera96@gmail.com>
2016-06-24 13:08:21 -0400

    fixed tag model

--- server/db/models/tag.js
+++ server/db/models/tag.js
@@ -3,7 +3,9 @@ var Sequelize= require('sequelize');
 var db= require('../_db');
 
 module.exports= db.define('tags', {
-	name: Sequelize.STRING,
-	allowNull: false
+	name:{
+		type: Sequelize.STRING,
+		allowNull: false
+	}
 });
 //some comment
