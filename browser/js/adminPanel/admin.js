217830733a5c2731f298cd78f3933b34a5ce4076
Angel <rriera96@gmail.com>
2016-06-25 16:33:12 -0400

    admin.topics state created

--- browser/js/adminPanel/admin.js
+++ browser/js/adminPanel/admin.js
@@ -2,15 +2,20 @@ app.config(function ($stateProvider) {
 
     $stateProvider.state('admin', {
         url: '/admin',
-        templateUrl: 'js/adminPanel/admin.html',
+        templateUrl: 'js/adminPanel/templates/admin.html',
         controller: 'AdminCtrl'
     });
 
+    $stateProvider.state('admin.topics', {
+        url: '/topics',
+        templateUrl: 'js/adminPanel/templates/topics.html'
+    });
 });
+
 
 app.controller('AdminCtrl', function ($scope) {
 
-    
+
 
 
 
