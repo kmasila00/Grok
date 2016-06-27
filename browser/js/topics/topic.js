0772c2a1dfa95dca31a8602980edc3add85e5e30
jszymanowski <j.szymanowski@gmail.com>
2016-06-27 19:05:02 -0400

    Completed tag addition and filtering

--- browser/js/topics/topic.js
+++ browser/js/topics/topic.js
@@ -18,6 +18,7 @@ app.config(function ($stateProvider) {
 app.controller('TopicCtrl', function ($scope, TopicFactory, topic, VoteFactory, AuthService, $uibModal, $log) {
 
   $scope.topic = topic;
+  $scope.resources = $scope.topic.resources;
 
   $scope.isLoggedIn = AuthService.isAuthenticated();
   $scope.showPlans = false;
@@ -31,6 +32,39 @@ app.controller('TopicCtrl', function ($scope, TopicFactory, topic, VoteFactory, 
     $scope.showResources = !$scope.showResources;
   }
 
+  // Tags
+  $scope.tagDisplay = {};
+
+  // -- get all unique tags, display all by default
+  $scope.topic.resources.forEach( function(resource) {
+    resource.display = true; // initially show all tags on the DOM
+    resource.tags.forEach( function(tag) {
+      if(!$scope.tagDisplay[tag.name]) $scope.tagDisplay[tag.name] = true;
+    });
+  });
+
+  $scope.showAllTags = function() {
+    for(var tagName in $scope.tagDisplay) $scope.tagDisplay[tagName] = true;
+    $('.btn-tag-filter').addClass('active');
+    refilterTags();
+  }
+
+  $scope.toggleTagFilter = function(tagName, $event) {
+    $($event.currentTarget).toggleClass('active');
+    $scope.tagDisplay[tagName] = !$scope.tagDisplay[tagName];
+    refilterTags();
+  }
+
+  function refilterTags() {
+    $scope.topic.resources.forEach( function(resource) {
+      resource.display = true;
+      resource.tags.forEach( function(tag) {
+        if(!$scope.tagDisplay[tag.name]) resource.display = false;
+      });
+    });
+  }
+
+  // -- add a tag to a resource
   $scope.addTag = function(resourceId) {
     var addTagModal = $uibModal.open({
       animation: true, // ??
@@ -44,14 +78,14 @@ app.controller('TopicCtrl', function ($scope, TopicFactory, topic, VoteFactory, 
 
     addTagModal.result
     .then(function (newTag) {
-      // updat DOM
+      // update DOM
       $scope.topic.resources.forEach( function(resource) {
         if(resource.id === resourceId) {
           resource.tags.push({name: newTag});
         }
       });
+      if(!$scope.tagDisplay[newTag]) $scope.tagDisplay[newTag] = true;
     });
-
   }
 
   // Voting
