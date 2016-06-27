e27f0ca13c714b7fe2f750173c804ad2bdea22a6
Kelvin Masilamani <kmasila00@gmail.com>
2016-06-27 12:24:10 -0400

    front end for forms is done

--- browser/js/topics/topics.js
+++ browser/js/topics/topics.js
@@ -20,3 +20,53 @@ app.controller('TopicsCtrl', function ($scope, TopicFactory, topics) {
   $scope.topics = topics;
 
 });
+
+//MODAL FORMS!!!!
+
+app.controller('ModalCtrl', function ($scope, $uibModal, $log, $rootScope) {
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (name, size) {
+
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+          modalName: function(){
+            return name;
+          }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+
+});
+
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+
+app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, modalName, $rootScope) {
+
+  $scope.name = modalName;
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
