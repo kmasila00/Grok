93fcf6aacf6a415002f08cfb5de5976d3edbc8e3
jszymanowski <j.szymanowski@gmail.com>
2016-06-30 10:37:03 -0400

    Added sidebar

---
+++ tools/chrome-extension/app/scripts.babel/directives/sidebar.directive.js
@@ -0,0 +1,13 @@
+app.directive('sidebar', function ($state) {
+
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'views/sidebar.html',
+        link: function (scope) {
+
+        }
+
+    };
+
+});
--- tools/chrome-extension/app/scripts.babel/states.js
+++ tools/chrome-extension/app/scripts.babel/states.js
@@ -2,9 +2,17 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
   $urlRouterProvider.otherwise('/');
 
-  $stateProvider.state('main', {
+  $stateProvider.state('home', {
     url: '/',
     templateUrl: 'views/home.html',
+    controller: 'MainController',
+    resolve: {
+    }
+  });
+
+  $stateProvider.state('addResource', {
+    url: '/',
+    templateUrl: 'views/addResource.html',
     controller: 'MainController',
     resolve: {
       topics: function(MainFactory) {
