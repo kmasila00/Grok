8ced6cfaeb4903d439531106be748ae53906f186
Kelvin Masilamani <kmasila00@gmail.com>
2016-07-01 11:51:21 -0400

    plan modal ready to be wired up

--- browser/js/modalWindows/modal.controller.js
+++ browser/js/modalWindows/modal.controller.js
@@ -146,8 +146,8 @@ app.controller('ModalPlanCtrl', function ($scope, $uibModal, $log, $rootScope) {
 
     var modalInstance = $uibModal.open({
       animation: $scope.animationsEnabled,
-      templateUrl: './js/modalWindows/addTPRModal.html',
-      controller: 'ModalInstanceFormCtrl',
+      templateUrl: './js/modalWindows/addPlanModal.html',
+      controller: 'ModalInstancePlanCtrl',
       size: size,
       resolve: {
           modalName: function(){
@@ -175,39 +175,20 @@ app.controller('ModalInstancePlanCtrl', function ($scope, $rootScope, $uibModalI
   $scope.name = modalName;
   $scope.showit = false;
 
-  if($scope.name === 'Resource')
-    $scope.showit = true;
+  //fetch all plans for the USER for a TOPIC
+  PlanFactory.fetchPlansByUser(userId)
+  .then(function(plansForUser){
+    $scope.userPlans = [];
+    plansForUser.forEach(function(elem){
+      if(elem.topicId === topic.id)
+        $scope.userPlans.push(elem);
+    })
+  });
 
-  $scope.add = function(){
-    //checks to see if form is not empty
-    if($scope.form.formTitle.$dirty && $scope.form.formDesc.$dirty){
-        //checks to see what we are creating
-        if($scope.name === 'Topic'){
-          TopicFactory.addNewTopic($scope.TRPname, $scope.TRPdesc)
-          .then(function(newTopic){
-          })
-          $uibModalInstance.dismiss();
-        } else if ($scope.name === 'Resource'){
-          //this is supposed to check to make sure url field and type is not empty
-          //have to edit to make sure all fields are required and get rid of description
-          if($scope.form.formUrl.$dirty){
-            ResourceFactory.addNewResource($scope.TRPname, $scope.resourceUrl, $scope.resourceType, $rootScope.topicId)
-            .then(function(resource){
-            })
-            $uibModalInstance.dismiss();
-          } else {
-            $scope.showError = true;
-          }
-        } else {
-          //uses the rootscope from topic.js to create a plan that has the topic id
-          PlanFactory.addNewPlan($scope.TRPname, $scope.TRPdesc, $rootScope.topicId)
-          .then(function(newPlan){
-          })
-          $uibModalInstance.dismiss();
-        }
-    } else {
-      $scope.showError = true;
-    }
+    //adds resource to a specific plan
+  $scope.addToPlan = function(resourceId){
+    PlanFactory.addResourceToPlan($scope.selectedPlan.id, resourceId)
+    .then();
   }
 
   $scope.cancel = function () {
